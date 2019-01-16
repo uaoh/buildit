7b7814b30fd5c83c394c1532652877a5949c8949
Aapi Hämäläinen <aapi.hamalainen@jollamobile.com>
2019-01-16 11:59:15 +0200

    JB-44043 move scripts out of user-data
    Co-authored-by: Johannes Lahti <johannes.lahti@jollamobile.com>

--- buildit.sh
+++ buildit.sh
@@ -1,73 +1,251 @@
 #!/bin/bash
+set -eu
 
-export VM=ci
-export VG=virt
+d_my="$( dirname "$( readlink -e "${0}" )" )"
+d_mnt="$( mktemp -d )"
+#d_tmp="$( mktemp -d )"
 
-function clean_old_vm(){
-    sudo virsh destroy $VM
-    sudo virsh undefine $VM
+d_tmp="${d_my}/tmp"
+test -e "${d_tmp}" && {
+    echo "${d_tmp} exists, stop."
+    exit 42
+}
+mkdir -p "${d_tmp}"
+
+
+cleanup()
+{
+    rm -rf "${d_tmp}"
+    mountpoint -q "${d_mnt}" && umount "${d_mnt}" || :
+    test -d "${d_mnt}" && rmdir "${d_mnt}"
+}
+trap cleanup EXIT
+
+
+usage()
+{
+    bn="$( basename "${0}" )"
+    bs="${bn//?/ }"
+    {
+	echo "Usage: ${bn} [-l|<resource_set>]"
+	echo "       ${bs} [-vm <vm_prefix>]"
+	echo "       ${bs} [-vg <vol_group>]"
+    } >&2
+    exit 1
 }
 
-function setup_storage(){
-    if [[ ! -e /dev/mapper/${VG}-thinpool-tpool ]]; then
-        echo No thin pool. Creating...
-        lvcreate -L 1G -T ${VG}/thinpool
+VM=ci
+VG=ubuntu-vg
+
+res_sets=(
+    $(
+	rd="$( readlink -e "${d_my}/res" )"
+	find "${rd}" -regex "${rd}/[^/]+/scripts/[0-9]+-[a-z_-]+\.sh" -print0 |
+	    xargs -0 -n 1 -r dirname -z |
+	    xargs -0 -n 1 -r dirname -z |
+	    sort -zu |
+	    xargs -0 -n 1 -r basename
+    )
+)
+res_set=
+
+while test ${#} -gt 0
+do
+    case "${1}" in
+	-l)
+	    for ss in "${res_sets[@]}"
+	    do
+		echo "${ss}"
+	    done
+	    shift
+	    exit 0
+	    ;;
+	-vm)
+	    [[ "${2:-}" =~ ^[^-] ]] || {
+		echo "Missing value for -vm"
+		exit 42
+	    }
+	    VM="${2}"
+	    shift 2
+	    ;;
+	-vg)
+	    [[ "${2:-}" =~ ^[^-] ]] || {
+		echo "Missing value for -vg"
+		exit 42
+	    }
+	    VG="${2}"
+	    shift 2
+	    ;;
+	*)
+	    c="$( basename "${1}" )"
+	    shift
+	    for ss in "${res_sets[@]}"
+	    do
+		test "${c}" = "${ss}" && {
+		    res_set="${c}"
+		    break
+		}
+	    done
+	    test -z "${res_set}" && {
+		echo "No such script set '${c}'"
+		exit 42
+	    }
+	    ;;
+    esac
+done
+
+while test -z "${res_set}"
+do
+    select res_set in "${res_sets[@]}"
+    do
+	test -z "${res_set}" ||
+	    break
+    done
+done
+
+export VM VG
+
+mkdir "${d_tmp}/share"
+rsync -r -L -p -t -o -g -v "${d_my}/res/${res_set}/" "${d_tmp}/share/"
+rsync -r -L -p -t -o -g -v "${d_my}/res/common/" "${d_tmp}/share/"
+
+
+clean_old_vm()
+{
+    echo "*** Cleaning old VM ***"
+    virsh destroy "$VM" || :
+    virsh undefine "$VM" || :
+}
+
+
+setup_storage()
+{
+    echo "*** Setting up storage ***"
+    if
+	! lvs -S 'lv_name = thinpool' -o lv_name --noheading |
+	    grep -q "^  thinpool$"
+    then
+        echo "No thin pool. Creating..."
+        lvcreate -L 1G -T "${VG}/thinpool"
     fi
-    if [[ ! -e /dev/${VG}/${VM}-root ]]; then
-        echo No root fs
+    if
+	[[ ! -e "/dev/${VG}/${VM}-root" ]]
+    then
+        echo "No root fs" >&2
         exit 1
     fi
-      
-    lvremove -f /dev/${VG}/${VM}-root-snap
-    lvcreate --snapshot --name ${VM}-root-snap --size 5G /dev/${VG}/${VM}-root
+
+    lvremove -f "/dev/${VG}/${VM}-root-snap" || :
+    lvcreate --snapshot --name "${VM}-root-snap" --size 5G "/dev/${VG}/${VM}-root"
     # Make thin data and swap LVs
-    lvremove -f /dev/${VG}/${VM}-data
-    lvremove -f /dev/${VG}/${VM}-swap
-    lvcreate --name ${VM}-data -V 10G ${VG}/thinpool
-    lvcreate --name ${VM}-swap -V 1G ${VG}/thinpool
+    lvremove -f "/dev/${VG}/${VM}-data"
+    lvremove -f "/dev/${VG}/${VM}-swap"
+    lvcreate --name "${VM}-data" -V 10G "${VG}/thinpool"
+    lvcreate --name "${VM}-swap" -V 1G "${VG}/thinpool"
+    mkswap "/dev/${VG}/${VM}-swap"
 }
 
-function mount_guest(){
-    mount /dev/${VG}/${VM}-root-snap /mnt
+
+mount_guest()
+{
+    echo "*** Mounting guest ***"
+    mount "/dev/${VG}/${VM}-root-snap" "${d_mnt}"
 }
 
-function fixup_snapshot() {
-    cat <<EOF > /mnt/etc/fstab
-/dev/vda        /       ext4    rw,noatime,user_xattr,acl,barrier=1,data=ordered        0       1
+
+fixup_snapshot()
+{
+    echo "*** Fixing up snapshot ***"
+    cat <<EOF > "${d_mnt}/etc/fstab"
+/dev/vda        /       ext4    rw,noatime,user_xattr,acl,barrier=1,data=ordered                                  0  1
 EOF
-    chroot /mnt systemctl enable cloud-init.service
-    chroot /mnt passwd
-    # set passwd until cloud-init runs
+    chroot "${d_mnt}" systemctl enable cloud-init.service
+    chroot "${d_mnt}" systemctl enable cloud-config.service
+    chroot "${d_mnt}" systemctl enable cloud-final.service
+
+    echo "gpgcheck      = 0" >> "${d_mnt}/etc/zypp/zypp.conf"
+    for f in "${d_mnt}"/etc/zypp/repos.d/openSUSE-*.repo
+    do
+	sed -ri '/^\s*priority\s*=\s*[0-9]+\s*/ d' "${f}"
+	echo "priority = 99" >> "${f}"
+    done
 }
 
-function get_kernel() {
-    rm -rf /kvmboot/${VM}
-    mkdir -p /kvmboot/${VM}
-    cp /mnt/boot/vmlinuz-*  /kvmboot/${VM}/
-    cp /mnt/boot/initrd-*  /kvmboot/${VM}/
+
+get_kernel()
+{
+    echo "*** Getting the kernel ***"
+    rm -rf "/kvmboot/${VM}"
+    mkdir -p "/kvmboot/${VM}"
+    cp "${d_mnt}"/boot/vmlinuz-* "/kvmboot/${VM}/"
+    cp "${d_mnt}"/boot/initrd-* "/kvmboot/${VM}/"
 }
 
-function umount_guest(){
-    umount /mnt
-}
-function create_config_iso(){
-    cd /mer/jolla/devel/infra/deploy/cc-iso/
-    genisoimage -o ${VM}_config.iso -V cidata -r -J meta-data user-data network-config
 
-}
-function create_vm() {
-    sudo virt-install --name ${VM} --memory 512 --os-type linux \
-         -w network=default,model=virtio \
-         --disk vol=virsh_virt/${VM}-root-snap,bus=virtio,cache=none,format=raw --import \
-         --disk path=/dev/${VG}/${VM}-swap,bus=virtio,cache=none,format=raw \
-         --disk path=/dev/${VG}/${VM}-data,bus=virtio,cache=none,format=raw \
-         --graphics none \
-         --disk path=/mer/jolla/devel/infra/deploy/cc-iso/${VM}_config.iso,device=cdrom\
-         --boot kernel=/kvmboot/${VM}/vmlinuz-4.4.162-78-default,initrd=/kvmboot/${VM}/initrd-4.4.162-78-default,kernel_args="root=/dev/vda console=ttyS0"
+umount_guest()
+{
+    echo "*** Unmounting guest ***"
+    umount "${d_mnt}"
 }
 
-function show_console() {
-    sudo virsh console ${VM}
+
+create_network()
+{
+    echo "*** Creating the network ***"
+    nw_tmp="$( mktemp )"
+    cat <<EOF > "${nw_tmp}"
+<network>
+  <name>${VM}-network</name>
+  <forward mode="nat" />
+  <ip address="10.10.10.1" netmask="255.255.255.0">
+    <dhcp>
+      <range start="10.10.10.2" end="10.10.10.100" />
+      <host mac="da:9c:4a:e0:bb:9f" ip="10.10.10.2" />
+    </dhcp>
+  </ip>
+</network>
+EOF
+   virsh net-destroy "${VM}-network" || :
+   virsh net-undefine "${VM}-network" || :
+   virsh net-create "${nw_tmp}"
+   rm -f "${nw_tmp}"
+}
+
+
+create_config_iso()
+{
+    echo "*** Creating a config ISO ***"
+    genisoimage -o "${d_tmp}/${VM}_config.iso" -V cidata -r -J meta-data user-data
+}
+
+
+create_vm()
+{
+    boot=
+    boot="${boot}kernel=/kvmboot/${VM}/vmlinuz-4.4.165-81-default,"
+    boot="${boot}initrd=/kvmboot/${VM}/initrd-4.4.165-81-default,"
+    boot="${boot}kernel_args=\"root=/dev/vda console=ttyS0\""
+    disks=(
+        "path=/dev/${VG}/${VM}-root-snap,bus=virtio,cache=none,format=raw --import"
+        "path=/dev/${VG}/${VM}-swap,bus=virtio,cache=none,format=raw"
+        "path=/dev/${VG}/${VM}-data,bus=virtio,cache=none,format=raw"
+	"path=${d_tmp}/${VM}_config.iso,device=cdrom"
+    )
+
+    echo "*** Creating the VM ***"
+    virt-install --name "${VM}" --memory 2048 --os-type linux            \
+         -w network="${VM}-network",model=virtio,mac=da:9c:4a:e0:bb:9f   \
+         --filesystem "${d_tmp}/share",host0                             \
+         --graphics none                                                 \
+	 ${disks[@]/#/--disk }                                           \
+         --boot "${boot}"
+}
+
+
+show_console()
+{
+    echo "*** Showing the console ***"
+    virsh console "${VM}"
 }
 
 
@@ -77,10 +255,8 @@ mount_guest
 fixup_snapshot
 get_kernel
 umount_guest
-
+create_network
 create_config_iso
 create_vm
 #show_console
 exit 0
-
-
---
+++ rebuild-ci-root.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -eu
+
+OS="42.3"
+TARGET="${1:-/dev/sdb}"
+
+egrep -q "^${TARGET}" /etc/mtab && {
+    echo "${TARGET} appears in /etc/mtab, stop." >&2
+    exit 42
+}
+
+PKGS=(
+    bash-completion
+    cloud-init
+    cloud-init-config-suse
+    curl
+    emacs-nox
+    iptables
+    iputils
+    less
+    openssh
+    puppet
+    rsync
+    sudo
+    terminfo
+    wget
+    wicked
+)
+NOPKGS=(
+    gfxboot
+    gfxboot-branding-openSUSE
+    grub2
+    grub2-branding-openSUSE
+    lvm2
+    plymouth
+    plymouth-branding-openSUSE
+    rpcbind
+    ruby2.1
+)
+
+tmp_d="$( mktemp -d )"
+tmp_mnt="${tmp_d}/mnt"
+tmp_rootfs="${tmp_d}/rootfs"
+dsc="${tmp_d}/kiwi-descriptions/suse/x86_64/suse-leap-${OS}-JeOS/"
+
+cleanup()
+{
+    umount "${tmp_mnt}"
+    rm -rf "${tmp_d}"
+}
+trap cleanup EXIT
+
+zypper ar -f http://download.opensuse.org/repositories/Virtualization:/Appliances:/Builder/openSUSE_Leap_42.3/Virtualization:Appliances:Builder.repo || :
+zypper in --no-recommends python3-kiwi unzip curl checkmedia
+
+mkdir -p "${tmp_rootfs}"
+mkdir -p "${tmp_mnt}"
+mount "${TARGET}" "${tmp_mnt}"
+
+cd "${tmp_d}"
+git clone https://github.com/SUSE/kiwi-descriptions
+
+kiwi-ng                         \
+    --logfile=kiwi_${OS}.log    \
+    --debug                     \
+    system prepare              \
+    --description="${dsc}"      \
+    --root="${tmp_rootfs}"      \
+    --allow-existing-root       \
+    --ignore-repos              \
+    --add-repo=http://download.opensuse.org/distribution/leap/${OS}/repo/oss/,yast2,"openSUSE-${OS}_OSS",1,true          \
+    --add-repo=http://download.opensuse.org/distribution/leap/${OS}/repo/non-oss/,yast2,"openSUSE-${OS}_Non-OSS",1,true  \
+    --add-repo=http://download.opensuse.org/update/leap/${OS}/oss/,rpm-md,"openSUSE-${OS}_Update",1,true                 \
+    --add-repo=http://download.opensuse.org/update/leap/${OS}/non-oss/,rpm-md,"openSUSE-${OS}_Update-Non-OSS",1,true     \
+    "${NOPKGS[@]/#/--delete-package=}" "${PKGS[@]/#/--add-package=}"
+
+rsync -HAXav --numeric-ids --delete "${tmp_rootfs}/" "${tmp_mnt}"
