5e1c086c6108e82b4634bc6cb3aafcd8cd01c241
Aapi Hämäläinen <aapi.hamalainen@jollamobile.com>
2019-01-16 11:59:48 +0200

    JB-44043 add resource set for installing 2.9
    Co-authored-by: Johannes Lahti <johannes.lahti@jollamobile.com>

---
+++ res/install_29/scripts/0030-db_populate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+cd /srv/www/obs/api
+
+cat <<EOF > config/database.yml
+production:
+  adapter: mysql2
+  database: api_production
+  username: obs
+  password: skytree
+  encoding: utf8
+  timeout: 15
+  pool: 30
+EOF
+chown root:www config/database.yml
+chmod 0640 config/database.yml
+
+# Adding our default archs. Maybe just write the whole configuration.xml?
+sed -i "s/armv7l i586/armv7el armv8el i586/" db/seeds.rb
+
+# TODO: drop me!
+patch -p1 < <( sed -r 's@src/api/db/@db/@g' /host/0001-fix-structure.sql.patch )
+
+RAILS_ENV="production" rake db:setup writeconfiguration ts:index ts:start
---
+++ res/install_29/scripts/0070-obs_fe_seed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+db="api_production"
+
+if
+    test -r "/host/${db}.sql"
+then
+    echo "Importing ${db}.sql"
+    mysql -u root -p'skytree' "${db}" < "/host/${db}.sql"
+    echo "Imported ${db}.sql"
+else
+    echo "No SQL dump for ${db}, ignoring."
+fi
+
+if
+    test -d /host/sql
+then
+    {
+	find /host/sql -name "${db}.patch-*.sql" -print0 |
+            sort -zn |
+            xargs -0 -r -n 1 -I @ cat @
+    } | mysql -u root -p'skytree' "${db}"
+fi
